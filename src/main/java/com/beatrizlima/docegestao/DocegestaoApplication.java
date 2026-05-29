988865a98f1d3ce30678793fbdd5834fd1e88fac
beatrizlima-tech <bealima.tech@gmail.com>
2026-05-29 18:21:06 -0300

    chore: estrutura inicial do projeto

---
+++ src/main/java/com/beatrizlima/docegestao/DocegestaoApplication.java
@@ -0,0 +1,13 @@
+package com.beatrizlima.docegestao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocegestaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocegestaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beatrizlima/docegestao/DocegestaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beatrizlima.docegestao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocegestaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
